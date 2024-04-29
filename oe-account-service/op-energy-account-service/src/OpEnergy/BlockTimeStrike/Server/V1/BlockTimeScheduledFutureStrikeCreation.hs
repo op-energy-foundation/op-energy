ea3dc0e86a2df046cd6727ba5ef5e189d89d8a5a
Dambaev Alexander <ice.redmine@gmail.com>
2024-04-29 23:29:41 +0800

    refactor: do not use hardcoded metrics

--- oe-account-service/op-energy-account-service/src/OpEnergy/BlockTimeStrike/Server/V1/BlockTimeScheduledFutureStrikeCreation.hs
+++ oe-account-service/op-energy-account-service/src/OpEnergy/BlockTimeStrike/Server/V1/BlockTimeScheduledFutureStrikeCreation.hs
@@ -14,15 +14,13 @@ import Control.Monad(forM_, when)
 
 import           Database.Persist.Postgresql
 import           Prometheus(MonadMonitor)
-import qualified Prometheus as P
 
 import           Data.OpEnergy.API.V1.Block
 import           Data.OpEnergy.Account.API.V1.BlockTimeStrike
 import           Data.OpEnergy.API.V1.Positive
 import           Data.OpEnergy.API.V1.Natural
-import           OpEnergy.Account.Server.V1.Class (AppT, State(..), runLogging)
+import           OpEnergy.Account.Server.V1.Class (profile, AppT, State(..), runLogging)
 import           OpEnergy.Account.Server.V1.Config
-import qualified OpEnergy.Account.Server.V1.Metrics as Metrics(MetricsState(..))
 import           Data.Text.Show
 
 -- | This function is being called when latest confirmed block had been discovered. You should expect, that it can be called by recieving notification from the blockspan service either at discover or at a time of connection of block time service to blockspan service. So it is expected, that it will run at each restart of the blockspan service or at reconnection to blockspan service or at blocktime service restart.
@@ -43,40 +41,37 @@ ensureFutureStrikeExistsAhead
      )
   => BlockHeader
   -> AppT m ()
-ensureFutureStrikeExistsAhead currentTip = do
+ensureFutureStrikeExistsAhead currentTip = profile "ensureFutureStrikeExistsAhead" $ do
   State{ config = Config
          { configBlockTimeFutureStrikeShouldExistsAheadCurrentTip = configBlockTimeFutureStrikeShouldExistsAheadCurrentTip
          , configBlockTimeStrikeFutureGuessMinimumBlockAheadCurrentTip = configBlockTimeStrikeFutureGuessMinimumBlockAheadCurrentTip
          }
        , accountDBPool = pool
-       , metrics = Metrics.MetricsState { Metrics.ensureFutureStrikeExistsAhead = ensureFutureStrikeExistsAhead
-                                        }
        } <- ask
-  P.observeDuration ensureFutureStrikeExistsAhead $ do
-    let minimumFutureStrikeHeight = blockHeaderHeight currentTip + naturalFromPositive configBlockTimeStrikeFutureGuessMinimumBlockAheadCurrentTip
-        maximumFutureStrikeHeight = blockHeaderHeight currentTip + naturalFromPositive configBlockTimeFutureStrikeShouldExistsAheadCurrentTip
-    runLogging $ $(logDebug) ("ensureFutureStrikeExistsAhead: currentTip: " <> tshow currentTip <>  ", min/max: " <> tshow [minimumFutureStrikeHeight, maximumFutureStrikeHeight])
-    nonExistentBlockHeights <- liftIO $ do
-      now <- getCurrentTime
-      flip runSqlPersistMPool pool $ do
-        futureStrikesE <- selectList
-          [ BlockTimeStrikeFutureBlock >=. minimumFutureStrikeHeight
-          , BlockTimeStrikeFutureBlock <=. maximumFutureStrikeHeight
-          ] []
-        let futureStrikes = List.map (\(Entity _ strike) -> blockTimeStrikeFutureBlock strike) futureStrikesE
-        let nonExistentStrikeHeights = List.filter
-              (\height-> height `notElem` futureStrikes)
-              [ minimumFutureStrikeHeight .. maximumFutureStrikeHeight ]
-        forM_ nonExistentStrikeHeights $ \height-> do
-          insert (BlockTimeStrikeFuture { blockTimeStrikeFutureBlock = height
-                                        , blockTimeStrikeFutureNlocktime =
-                                          fromIntegral
-                                          $ blockHeaderMediantime currentTip
-                                          + fromIntegral ((fromNatural (height - blockHeaderHeight currentTip)) * 600) -- assume 10 minutes time slices for nlocktime
-                                        , blockTimeStrikeFutureCreationTime = utcTimeToPOSIXSeconds now
-                                        })
+  let minimumFutureStrikeHeight = blockHeaderHeight currentTip + naturalFromPositive configBlockTimeStrikeFutureGuessMinimumBlockAheadCurrentTip
+      maximumFutureStrikeHeight = blockHeaderHeight currentTip + naturalFromPositive configBlockTimeFutureStrikeShouldExistsAheadCurrentTip
+  runLogging $ $(logDebug) ("ensureFutureStrikeExistsAhead: currentTip: " <> tshow currentTip <>  ", min/max: " <> tshow [minimumFutureStrikeHeight, maximumFutureStrikeHeight])
+  nonExistentBlockHeights <- liftIO $ do
+    now <- getCurrentTime
+    flip runSqlPersistMPool pool $ do
+      futureStrikesE <- selectList
+        [ BlockTimeStrikeFutureBlock >=. minimumFutureStrikeHeight
+        , BlockTimeStrikeFutureBlock <=. maximumFutureStrikeHeight
+        ] []
+      let futureStrikes = List.map (\(Entity _ strike) -> blockTimeStrikeFutureBlock strike) futureStrikesE
+      let nonExistentStrikeHeights = List.filter
+            (\height-> height `notElem` futureStrikes)
+            [ minimumFutureStrikeHeight .. maximumFutureStrikeHeight ]
+      forM_ nonExistentStrikeHeights $ \height-> do
+        insert (BlockTimeStrikeFuture { blockTimeStrikeFutureBlock = height
+                                      , blockTimeStrikeFutureNlocktime =
+                                        fromIntegral
+                                        $ blockHeaderMediantime currentTip
+                                        + fromIntegral ((fromNatural (height - blockHeaderHeight currentTip)) * 600) -- assume 10 minutes time slices for nlocktime
+                                      , blockTimeStrikeFutureCreationTime = utcTimeToPOSIXSeconds now
+                                      })
 
-        return nonExistentStrikeHeights
-    when (List.length nonExistentBlockHeights > 0) $ do
-      runLogging $ $(logInfo) ("ensureFutureStrikeExistsAhead: created future strikes: " <> (tshow nonExistentBlockHeights))
+      return nonExistentStrikeHeights
+  when (List.length nonExistentBlockHeights > 0) $ do
+    runLogging $ $(logInfo) ("ensureFutureStrikeExistsAhead: created future strikes: " <> (tshow nonExistentBlockHeights))
 
