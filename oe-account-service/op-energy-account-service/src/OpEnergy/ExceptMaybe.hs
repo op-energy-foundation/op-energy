43b10bb6616fd825d9325e4ccf004c169ba55a0f
Dambaev Alexander <ice.redmine@gmail.com>
2025-10-24 10:03:40 +0800

    feat: introduce eitherLogThrowOrReturn runExceptPrefixT for useful design patterns

--- oe-account-service/op-energy-account-service/src/OpEnergy/ExceptMaybe.hs
+++ oe-account-service/op-energy-account-service/src/OpEnergy/ExceptMaybe.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE TemplateHaskell            #-}
 module OpEnergy.ExceptMaybe
   ( exceptTMaybeT
+  , eitherLogThrowOrReturn
+  , runExceptPrefixT
   ) where
 
-import           Control.Monad.Trans.Except(ExceptT(..))
+import           Data.Text(Text)
+
+import           Control.Monad.Trans.Except (ExceptT(..), runExceptT)
+import           Control.Monad.Logger(logError)
+import           OpEnergy.Account.Server.V1.Class ( AppM, runLogging)
+import           Data.OpEnergy.API.V1.Error (throwJSON)
+import           Servant.Server.Internal.ServerError(err500)
+
 
 -- | this function wraps function, which returns value of type 'Maybe result'
 -- into ExceptT transformer by providing 'Left' value in case of function will
@@ -17,4 +27,28 @@ exceptTMaybeT left f = ExceptT $ do
   case v of
     Nothing -> return (Left left)
     Just some -> return (Right some)
+
+-- | In case of success this function will just return @r@
+-- In case of failure, it will log error reason and throw it as a JSON error.
+eitherLogThrowOrReturn
+  :: AppM (Either Text r)
+  -> AppM r
+eitherLogThrowOrReturn foo = do
+  r <- foo
+  case r of
+    Left reason-> do
+      runLogging $ $(logError) reason
+      throwJSON err500 reason
+    Right ret -> return ret
+
+-- | this function is basically an extension to @runExceptT@ with the addition
+-- of @prefix@ to the error reason in case of failure
+runExceptPrefixT
+  :: (Monad m)
+  => Text
+  -> ExceptT Text m r
+  -> m (Either Text r)
+runExceptPrefixT prefix payload = do
+  ret <- runExceptT payload
+  return $! either (\reason -> Left (prefix <> ": " <> reason)) Right ret
 
