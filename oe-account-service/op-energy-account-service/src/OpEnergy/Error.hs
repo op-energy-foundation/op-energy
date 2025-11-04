197f6495a7ba4078ce38b523cb157230a05ab31d
Dambaev Alexander <ice.redmine@gmail.com>
2025-11-05 01:23:30 +0800

    feat: Error.hs: introduce runExceptPrefixT and eitherException

--- oe-account-service/op-energy-account-service/src/OpEnergy/Error.hs
+++ oe-account-service/op-energy-account-service/src/OpEnergy/Error.hs
@@ -2,11 +2,18 @@
 {-# LANGUAGE FlexibleInstances #-}
 module OpEnergy.Error
   ( eitherThrowJSON
+  , runExceptPrefixT
+  , eitherException
   ) where
 
 import           Data.Text(Text)
+import qualified Data.Text as Text
+
 import           Data.Aeson (ToJSON)
 import           Control.Monad.Error.Class(MonadError)
+import           Control.Monad.Trans.Except(ExceptT, runExceptT)
+import           Control.Exception.Safe (SomeException)
+import qualified Control.Exception.Safe as E
 
 import           Servant(ServerError)
 import           Data.OpEnergy.API.V1.Error(throwJSON)
@@ -32,4 +39,37 @@ eitherThrowJSON handler payload = do
       (err, msg) <- handler reason
       throwJSON err msg
 
+
+-- | The goal of this function is to add prefix to the error reason:w
+-- example
+-- @ runExceptT "MyFunction" $ throwE "error" @ will return @Left "MyFunction: error"@
+runExceptPrefixT
+  :: Monad m
+  => Text
+  -> ExceptT Text m r
+  -> m (Either Text r)
+runExceptPrefixT prefix payload = do
+  eret <- runExceptT payload
+  return $! either
+    (\reason-> Left (prefix <> ": " <> reason))
+    Right
+    eret
+
+-- | this functions's goal is to handle possible exception into @Either@ type
+-- in order to wrap side-effectful routine into ExceptT transformer
+-- Example:
+-- @ eitherException $ readFile "/file/not/found" @
+eitherException
+  :: IO r
+  -> IO (Either Text r)
+eitherException next = do
+  !ret <- E.handle
+    (\(e::SomeException)->
+      return (Left (Text.pack (show e)))
+    )
+    (do
+      !ret <- next
+      return (Right ret)
+    )
+  return ret
 
