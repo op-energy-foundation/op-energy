4e2ea00bd7da25a46868a1a2790d9a3c28304905
Dambaev Alexander <ice.redmine@gmail.com>
2025-11-05 01:38:03 +0800

    feat: BlocktimeStrike.Server.V2: introduce V2 handlers entry point

---
+++ oe-account-service/op-energy-account-service/src/OpEnergy/BlockTimeStrike/Server/V2.hs
@@ -0,0 +1,42 @@
+{-- |
+ - This module is the top module of backend V1
+ -}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE DuplicateRecordFields      #-}
+module OpEnergy.BlockTimeStrike.Server.V2
+  ( blockTimeServer
+  )where
+
+import           Servant
+
+
+
+import           Data.OpEnergy.API.V1.Positive
+import qualified Data.OpEnergy.Account.API.V1.BlockTimeStrike as API
+import qualified Data.OpEnergy.Account.API.V1.FilterRequest as API
+import qualified Data.OpEnergy.Account.API.V1.PagingResult as API
+import           Data.OpEnergy.API.V1.Natural(Natural)
+import           OpEnergy.Account.Server.V1.Class (AppM, AppT)
+
+import           Data.OpEnergy.BlockTime.API.V2
+import qualified Data.OpEnergy.BlockTime.API.V2.BlockSpanTimeStrike
+                 as API
+import qualified OpEnergy.BlockTimeStrike.Server.V2.BlockSpanTimeStrikeService
+                 as BlockSpanTimeStrikeService
+
+blockTimeServer :: ServerT BlockTimeV2API (AppT Handler)
+blockTimeServer
+  =  (BlockSpanTimeStrikeService.getStrikes
+          :: Maybe (Positive Int)
+          -> Maybe (Natural Int)
+          -> Maybe (API.FilterRequest API.BlockTimeStrike API.BlockTimeStrikeFilter)
+          -> AppM (API.PagingResult API.BlockSpanTimeStrike)
+       )
+
