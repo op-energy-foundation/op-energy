3add66b8c95a225c95313ec20904318873fd4efc
Dambaev Alexander <ice.redmine@gmail.com>
2026-01-28 17:22:19 +0800

    refactor: remove BlockTimeStrikeGuessResult
    remove BlockTimeStrikeGuessResult leaving only BlockTimeStrikeGuess as
    they become identical

--- oe-account-service/op-energy-account-api/src/Data/OpEnergy/Account/API/V1/BlockTimeStrikeGuess.hs
+++ oe-account-service/op-energy-account-api/src/Data/OpEnergy/Account/API/V1/BlockTimeStrikeGuess.hs
@@ -10,8 +10,7 @@
 {-# LANGUAGE DerivingStrategies         #-}
 module Data.OpEnergy.Account.API.V1.BlockTimeStrikeGuess
   ( BlockTimeStrikeGuess(..)
-  , BlockTimeStrikeGuessResult(..)
-  , BlockTimeStrikeGuessResultFilter(..)
+  , BlockTimeStrikeGuessFilter(..)
   ) where
 
 import           Data.Swagger
@@ -69,75 +68,47 @@ defaultBlockTimeStrikeGuess = BlockTimeStrikeGuess
   , guess = def
   }
 
-data BlockTimeStrikeGuessResult = BlockTimeStrikeGuessResult
-  { person :: UUID Person
-  , strike :: BlockTimeStrike
-  , creationTime :: POSIXTime
-  , guess :: SlowFast
-  }
-  deriving (Show, Generic)
-instance ToJSON BlockTimeStrikeGuessResult
-instance ToSchema BlockTimeStrikeGuessResult where
-  declareNamedSchema proxy = genericDeclareNamedSchema defaultSchemaOptions proxy
-    & mapped.schema.type_ ?~ SwaggerObject
-    & mapped.schema.example ?~ toJSON defaultBlockTimeStrikeGuessResult
-    & mapped.schema.required .~
-      [ "person"
-      , "strike"
-      , "creationTime"
-      , "guess"
-      ]
-instance Default BlockTimeStrikeGuessResult where
-  def = defaultBlockTimeStrikeGuessResult
-defaultBlockTimeStrikeGuessResult :: BlockTimeStrikeGuessResult
-defaultBlockTimeStrikeGuessResult = BlockTimeStrikeGuessResult
-  { person = defaultUUID
-  , strike = def
-  , creationTime = defaultPOSIXTime
-  , guess = def
-  }
-
-data BlockTimeStrikeGuessResultFilter = BlockTimeStrikeGuessResultFilter
+data BlockTimeStrikeGuessFilter = BlockTimeStrikeGuessFilter
     -- person
-  { blockTimeStrikeGuessResultFilterPersonEQ              :: Maybe (UUID Person)
-  , blockTimeStrikeGuessResultFilterPersonNEQ             :: Maybe (UUID Person)
+  { blockTimeStrikeGuessFilterPersonEQ              :: Maybe (UUID Person)
+  , blockTimeStrikeGuessFilterPersonNEQ             :: Maybe (UUID Person)
     -- guess
-  , blockTimeStrikeGuessResultFilterGuessEQ               :: Maybe SlowFast
-  , blockTimeStrikeGuessResultFilterGuessNEQ              :: Maybe SlowFast
-    -- observedResult
-  , blockTimeStrikeGuessResultFilterObservedResultEQ      :: Maybe SlowFast
-  , blockTimeStrikeGuessResultFilterObservedResultNEQ     :: Maybe SlowFast
+  , blockTimeStrikeGuessFilterGuessEQ               :: Maybe SlowFast
+  , blockTimeStrikeGuessFilterGuessNEQ              :: Maybe SlowFast
+    -- observed
+  , blockTimeStrikeGuessFilterObservedResultEQ      :: Maybe SlowFast
+  , blockTimeStrikeGuessFilterObservedResultNEQ     :: Maybe SlowFast
     -- strike block height
-  , blockTimeStrikeGuessResultFilterStrikeBlockHeightGTE  :: Maybe BlockHeight
-  , blockTimeStrikeGuessResultFilterStrikeBlockHeightLTE  :: Maybe BlockHeight
-  , blockTimeStrikeGuessResultFilterStrikeBlockHeightEQ   :: Maybe BlockHeight
-  , blockTimeStrikeGuessResultFilterStrikeBlockHeightNEQ  :: Maybe BlockHeight
+  , blockTimeStrikeGuessFilterStrikeBlockHeightGTE  :: Maybe BlockHeight
+  , blockTimeStrikeGuessFilterStrikeBlockHeightLTE  :: Maybe BlockHeight
+  , blockTimeStrikeGuessFilterStrikeBlockHeightEQ   :: Maybe BlockHeight
+  , blockTimeStrikeGuessFilterStrikeBlockHeightNEQ  :: Maybe BlockHeight
     -- strike strikeMediantime
-  , blockTimeStrikeGuessResultFilterStrikeMediantimeGTE   :: Maybe POSIXTime
-  , blockTimeStrikeGuessResultFilterStrikeMediantimeLTE   :: Maybe POSIXTime
-  , blockTimeStrikeGuessResultFilterStrikeMediantimeEQ    :: Maybe POSIXTime
-  , blockTimeStrikeGuessResultFilterStrikeMediantimeNEQ   :: Maybe POSIXTime
+  , blockTimeStrikeGuessFilterStrikeMediantimeGTE   :: Maybe POSIXTime
+  , blockTimeStrikeGuessFilterStrikeMediantimeLTE   :: Maybe POSIXTime
+  , blockTimeStrikeGuessFilterStrikeMediantimeEQ    :: Maybe POSIXTime
+  , blockTimeStrikeGuessFilterStrikeMediantimeNEQ   :: Maybe POSIXTime
     -- sort
-  , blockTimeStrikeGuessResultFilterSort                  :: Maybe SortOrder
-  , blockTimeStrikeGuessResultFilterClass                 :: Maybe BlockTimeStrikeFilterClass
-  , blockTimeStrikeGuessResultFilterLinesPerPage          :: Maybe (Positive Int)
+  , blockTimeStrikeGuessFilterSort                  :: Maybe SortOrder
+  , blockTimeStrikeGuessFilterClass                 :: Maybe BlockTimeStrikeFilterClass
+  , blockTimeStrikeGuessFilterLinesPerPage          :: Maybe (Positive Int)
   }
   deriving (Eq, Show, Generic)
-instance Default BlockTimeStrikeGuessResultFilter where
-  def = defaultBlockTimeStrikeGuessResultFilter
-instance ToJSON BlockTimeStrikeGuessResultFilter where
+instance Default BlockTimeStrikeGuessFilter where
+  def = defaultBlockTimeStrikeGuessFilter
+instance ToJSON BlockTimeStrikeGuessFilter where
   toJSON = commonToJSON genericToJSON
   toEncoding = commonToJSON genericToEncoding
-instance FromJSON BlockTimeStrikeGuessResultFilter where
+instance FromJSON BlockTimeStrikeGuessFilter where
   parseJSON = commonParseJSON
-instance ToSchema BlockTimeStrikeGuessResultFilter where
+instance ToSchema BlockTimeStrikeGuessFilter where
   declareNamedSchema proxy = genericDeclareNamedSchema defaultSchemaOptions proxy
     & mapped.schema.description ?~ (Text.unlines
-      [ "This is the example of the public filter available for BlockTimeStrikeGuessResult"
+      [ "This is the example of the public filter available for BlockTimeStrikeGuess"
       , "each field is optional and can be provided in a combination of unique fields to build specific filter"
       ])
-    & mapped.schema.example ?~ toJSON defaultBlockTimeStrikeGuessResultFilter
-instance ToParamSchema BlockTimeStrikeGuessResultFilter where
+    & mapped.schema.example ?~ toJSON defaultBlockTimeStrikeGuessFilter
+instance ToParamSchema BlockTimeStrikeGuessFilter where
   toParamSchema v = mempty
     & type_ ?~ SwaggerString
     & format ?~ ( Text.unlines
@@ -147,10 +118,10 @@ instance ToParamSchema BlockTimeStrikeGuessResultFilter where
     where
       def1 :: Default a => Proxy a-> a
       def1 = def
-instance ToHttpApiData BlockTimeStrikeGuessResultFilter where
+instance ToHttpApiData BlockTimeStrikeGuessFilter where
   toUrlPiece v = toUrlPiece $ Text.decodeUtf8 $ BS.toStrict $ encode v
   toQueryParam v = toQueryParam $ Text.decodeUtf8 $ BS.toStrict $ encode v
-instance FromHttpApiData BlockTimeStrikeGuessResultFilter where
+instance FromHttpApiData BlockTimeStrikeGuessFilter where
   parseUrlPiece v = case Aeson.eitherDecodeStrict (Text.encodeUtf8 v) of
     Left some -> Left (Text.pack some)
     Right some -> Right some
@@ -158,23 +129,23 @@ instance FromHttpApiData BlockTimeStrikeGuessResultFilter where
     Left some -> Left (Text.pack some)
     Right some -> Right some
 
-defaultBlockTimeStrikeGuessResultFilter :: BlockTimeStrikeGuessResultFilter
-defaultBlockTimeStrikeGuessResultFilter =  BlockTimeStrikeGuessResultFilter
-  { blockTimeStrikeGuessResultFilterPersonEQ              = Just defaultUUID
-  , blockTimeStrikeGuessResultFilterPersonNEQ             = Just defaultUUID
-  , blockTimeStrikeGuessResultFilterGuessEQ               = Just Slow
-  , blockTimeStrikeGuessResultFilterGuessNEQ              = Just Fast
-  , blockTimeStrikeGuessResultFilterObservedResultEQ      = Just Slow
-  , blockTimeStrikeGuessResultFilterObservedResultNEQ     = Just Fast
-  , blockTimeStrikeGuessResultFilterStrikeBlockHeightGTE  = Just 1
-  , blockTimeStrikeGuessResultFilterStrikeBlockHeightLTE  = Just 1
-  , blockTimeStrikeGuessResultFilterStrikeBlockHeightEQ   = Just 1
-  , blockTimeStrikeGuessResultFilterStrikeBlockHeightNEQ  = Just 1
-  , blockTimeStrikeGuessResultFilterStrikeMediantimeGTE   = Just 1
-  , blockTimeStrikeGuessResultFilterStrikeMediantimeLTE   = Just 1
-  , blockTimeStrikeGuessResultFilterStrikeMediantimeEQ    = Just 1
-  , blockTimeStrikeGuessResultFilterStrikeMediantimeNEQ   = Just 1
-  , blockTimeStrikeGuessResultFilterSort                  = Just Descend
-  , blockTimeStrikeGuessResultFilterClass                 = Just defaultBlockTimeStrikeFilterClass
-  , blockTimeStrikeGuessResultFilterLinesPerPage          = Just 100
+defaultBlockTimeStrikeGuessFilter :: BlockTimeStrikeGuessFilter
+defaultBlockTimeStrikeGuessFilter =  BlockTimeStrikeGuessFilter
+  { blockTimeStrikeGuessFilterPersonEQ              = Just defaultUUID
+  , blockTimeStrikeGuessFilterPersonNEQ             = Just defaultUUID
+  , blockTimeStrikeGuessFilterGuessEQ               = Just Slow
+  , blockTimeStrikeGuessFilterGuessNEQ              = Just Fast
+  , blockTimeStrikeGuessFilterObservedResultEQ      = Just Slow
+  , blockTimeStrikeGuessFilterObservedResultNEQ     = Just Fast
+  , blockTimeStrikeGuessFilterStrikeBlockHeightGTE  = Just 1
+  , blockTimeStrikeGuessFilterStrikeBlockHeightLTE  = Just 1
+  , blockTimeStrikeGuessFilterStrikeBlockHeightEQ   = Just 1
+  , blockTimeStrikeGuessFilterStrikeBlockHeightNEQ  = Just 1
+  , blockTimeStrikeGuessFilterStrikeMediantimeGTE   = Just 1
+  , blockTimeStrikeGuessFilterStrikeMediantimeLTE   = Just 1
+  , blockTimeStrikeGuessFilterStrikeMediantimeEQ    = Just 1
+  , blockTimeStrikeGuessFilterStrikeMediantimeNEQ   = Just 1
+  , blockTimeStrikeGuessFilterSort                  = Just Descend
+  , blockTimeStrikeGuessFilterClass                 = Just defaultBlockTimeStrikeFilterClass
+  , blockTimeStrikeGuessFilterLinesPerPage          = Just 100
   }
