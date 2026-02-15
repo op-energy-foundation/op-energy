f15c23b4cbf51dc0c8f6397754232a94420816dd
Dambaev Alexander <ice.redmine@gmail.com>
2026-02-15 13:31:01 +0800

    Revert "feat: implementation POST /api/v2/strikes/blockrate/strike/{StrikeBlockHeight}/{StrikeMediantime}"
    This reverts commit 532d55e26d75e202a43a091e5ae69d2c7ce7fe8f.

--- oe-account-service/op-energy-account-service/src/OpEnergy/BlockTimeStrike/Server/V2/StrikeAPI.hs
+++ oe-account-service/op-energy-account-service/src/OpEnergy/BlockTimeStrike/Server/V2/StrikeAPI.hs
@@ -12,15 +12,12 @@ import           Data.OpEnergy.API.V1.Natural(Natural)
 import           Data.OpEnergy.API.V1.Positive
 import qualified Data.OpEnergy.API.V1.Block as BlockV1
 
-import qualified Data.OpEnergy.Account.API.V1.Account as AccountV1
 import           OpEnergy.Account.Server.V1.Class (AppM, AppT)
 
 import qualified Data.OpEnergy.BlockTime.API.V2.BlockSpanTimeStrike
                  as API
 import qualified OpEnergy.BlockTimeStrike.Server.V2.StrikeAPI.GetStrike
                  as GetStrike
-import qualified OpEnergy.BlockTimeStrike.Server.V2.StrikeAPI.Create
-                 as Create
 import qualified OpEnergy.BlockTimeStrike.Server.V2.GuessAPI
                  as GuessAPI
 import qualified OpEnergy.BlockTimeStrike.Server.V2.StrikeGuessesAPI
@@ -34,14 +31,6 @@ handlers
           -> Maybe (Positive Int)
           -> AppM API.BlockSpanTimeStrike
         )
-
-  :<|> ( Create.createHandler
-         :: AccountV1.AccountToken -- require authentication
-         -> BlockV1.BlockHeight
-         -> Natural Int
-         -> Maybe (Positive Int)
-         -> AppM  API.BlockSpanTimeStrike
-       )
 
   :<|> ( GuessAPI.handlers
          :: BlockV1.BlockHeight
