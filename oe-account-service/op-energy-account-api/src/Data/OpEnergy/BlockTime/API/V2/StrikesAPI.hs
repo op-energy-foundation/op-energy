3d7afbaaee347c79fee87e92e4bbaa7bd2d6ae73
Dambaev Alexander <ice.redmine@gmail.com>
2026-05-14 23:04:24 +0800

    feat: introduce /api/v2/blockrate/strikes/guesses
    to get user's guesses for all strikes

--- oe-account-service/op-energy-account-api/src/Data/OpEnergy/BlockTime/API/V2/StrikesAPI.hs
+++ oe-account-service/op-energy-account-api/src/Data/OpEnergy/BlockTime/API/V2/StrikesAPI.hs
@@ -8,6 +8,7 @@ import           Servant.API
 
 import           Data.OpEnergy.API.V1.Natural
 import           Data.OpEnergy.API.V1.Positive
+import           Data.OpEnergy.Account.API.V1.Account
 import           Data.OpEnergy.Account.API.V1.PagingResult
 import           Data.OpEnergy.Account.API.V1.FilterRequest
 import           Data.OpEnergy.Account.API.V1.BlockTimeStrike
@@ -15,6 +16,9 @@ import           Data.OpEnergy.Account.API.V1.BlockTimeStrike
                    , BlockTimeStrikeFilter
                    )
 import           Data.OpEnergy.BlockTime.API.V2.BlockSpanTimeStrike
+
+import           Data.OpEnergy.BlockTime.API.V2.BlockSpanTimeStrikeGuess
+import           Data.OpEnergy.BlockTime.API.V2.BlockSpanTimeStrikeGuessFilter
 
 type StrikesAPI
   =    QueryParam'
@@ -54,4 +58,45 @@ type StrikesAPI
                    \by strike id in descending order. \
                    \(ie, from newer to older)"
     :> Get '[JSON] (PagingResult BlockSpanTimeStrike)
+
+  :<|> "guesses"
+    :> Header'
+       '[ Required
+        , Strict
+        , Description "Account token gotten from /login or /register"
+        ]
+       "Authorization"
+       AccountToken -- require authentication
+    :> QueryParam'
+       '[ Optional
+        , Strict
+        , Description "defines block span size"
+        ]
+       "spanSize"
+       (Positive Int)
+    :> QueryParam'
+       '[ Optional
+        , Strict
+        , Description "defines page count to get"
+        ]
+       "page"
+       (Natural Int)
+    :> QueryParam'
+       '[ Optional
+        , Strict
+        , Description "possible filter as a string in JSON format. you can \
+                      \pass any combination of it's unique fields to build a \
+                      \filter. Available filter options are listed in the \
+                      \format of current field. Meaning of fields' suffixes: \
+                      \'GTE' - 'great-than-or-equal', \
+                      \'LTE'- 'less-than-or-equal', \
+                      \'EQ' - equal, 'NEQ' - 'not equal'. \
+                      \'sort' field can have those values: 'descend', \
+                      \'ascend'. 'class' field can have those values: \
+                      \'guessable', 'outcomeKnown', 'outcomeUnknown'"
+        ]
+       "filter"
+       (FilterRequest BlockSpanTimeStrikeGuess BlockSpanTimeStrikeGuessFilter)
+    :> Description "returns list of user's guesses for all strikes"
+    :> Get '[JSON] (PagingResult BlockSpanTimeStrikeGuess)
 
