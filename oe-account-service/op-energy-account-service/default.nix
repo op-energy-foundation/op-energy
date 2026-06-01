44f25e3a5cea5df0e8ce443f2731e7b09964b9d4
Dambaev Alexander <ice.redmine@gmail.com>
2026-06-01 13:48:55 +0800

    fix to improve debugging

--- oe-account-service/op-energy-account-service/default.nix
+++ oe-account-service/op-energy-account-service/default.nix
@@ -1,6 +1,8 @@
+{ GIT_COMMIT_HASH ? ""
+}:
 let
   stable = import ../../nixpkgs.nix;
-  overlay = import ../../overlay.nix { GIT_COMMIT_HASH = "";};
+  overlay = import ../../overlay.nix { GIT_COMMIT_HASH = GIT_COMMIT_HASH;};
   pkgs = (import stable {
     overlays = [ overlay ];
   }); # first, load the nixpkgs with system-wide overlays
