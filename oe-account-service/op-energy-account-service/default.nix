fcd810866b2246a570dc78c92b3b3ed2703e53aa
Dambaev Alexander <ice.redmine@gmail.com>
2026-06-01 10:20:30 +0800

    feat: provide default.nix for op-energy-account-service

---
+++ oe-account-service/op-energy-account-service/default.nix
@@ -0,0 +1,8 @@
+let
+  stable = import ../../nixpkgs.nix;
+  overlay = import ../../overlay.nix { GIT_COMMIT_HASH = "";};
+  pkgs = (import stable {
+    overlays = [ overlay ];
+  }); # first, load the nixpkgs with system-wide overlays
+
+in pkgs.op-energy-account-service
